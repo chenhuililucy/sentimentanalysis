fe142596b23dfb014f40fd36cc0a0dd9b20d0b60
Lucy Li Chenhui <48955216+chenhuililucy@users.noreply.github.com>
2020-06-05 23:11:18 +0800

    Add files via upload

--- regression.py
+++ regression.py
@@ -3,11 +3,12 @@ import csv
 import os
 import re
 
-dir1= "/Users/lucy/Desktop/assortedcodes/5df8773df51c8ae4.csv"
-dir2= "/Users/lucy/Desktop/assortedcodes/builddic/vector1.csv"
+#dir1= "/Users/lucy/Desktop/assortedcodes/5df8773df51c8ae4.csv"
+dir2= "/Users/lucy/Desktop/assortedcodes/regression2.csv"
 
 allfiles="/Users/lucy/Desktop/others/allfiles"
 
+from collections import defaultdict
 
 """ 
 Part 1: retrieve all filedates
@@ -18,7 +19,47 @@ item2="FILER"
 item3="COMPANY CONFORMED NAME:"
 item=item.lower()
 
-s=set()
+dfin="/Users/lucy/Desktop/assortedcodes/91e17c1ef257fb31.csv"
+
+
+d={}
+with open(dfin,"r") as posfile: 
+    #print("u")
+    records=csv.reader(posfile)
+    i=0
+    for row in records:
+        if i==0: 
+            i+=1
+            continue
+        cik=row[12]
+        if len(cik)<10:
+            p=10-len(cik)
+            cik="0"*p+cik
+            print("y")
+        datadate=row[2]
+        ni=row[11]
+        at=row[9]
+        #print(ni)
+        #if ni.isnumeric() and at.isnumeric():
+        try:
+            float(ni)
+            float(at)
+            if float(at)!=0:
+                ROA=float(ni)/float(at)
+                d.update({(cik,datadate):ROA})
+        except ValueError:
+            print ("Not a float")
+            print(ni)
+            print(at)
+
+        #print(ROA)
+       # i+=1
+
+    posfile.close()
+
+print(d)
+
+s=defaultdict(int)
 l1=[]
 l2=[]
 l3=[]
@@ -26,27 +67,44 @@ l4=[]
 l5=[]
 l6=[]
 l7=[]
+l8=[]
 
 with open(dir2,"r") as posfile: 
     records=csv.reader(posfile)
     i=0
+    
+    
     for row in records:
         if i==0: 
-            pass
+            i+=1 
+            continue
         i+=1 
         year=row[1]
-        cik=row[2]
-        s.add((year,cik))
-        
-    l1.append(row[0])
-    l2.append(row[1])
-    l3.append(row[2])
-    l4.append(int(row[3])/int(row[7]))
-    l5.append(int(row[4])/int(row[7]))
-    l6.append(int(row[5])/int(row[7]))
-    l7.append(int(row[6])/int(row[7]))
+        cikoriginal=row[2]
+        if len(cikoriginal)<10:
+            p=10-len(cikoriginal)
+            cik="0"*p+cikoriginal
+        if d.get((cik,year)):
+            ROA=d[(cik,year)]
+            l7.append(ROA)
+        else: 
+            l7.append(".")
 
-f=[]
+        s.update({(cikoriginal,year):i-2})
+        
+        l1.append(row[0])
+        l2.append(row[1])
+        l3.append(row[2])
+        l4.append(float(row[3])/float(row[5]))
+        l5.append(float(row[4])/float(row[5]))
+        l6.append(row[6])
+    posfile.close()
+    #l6.append(int(row[5])/int(row[7]))
+    #l7.append(int(row[6])/int(row[7]))
+
+i=i+1
+f=[0]*i
+
 for root, dirs, files in os.walk("/Users/lucy/Desktop/others/allfiles"):
     for file in files:
         
@@ -55,8 +113,9 @@ for root, dirs, files in os.walk("/Users/lucy/Desktop/others/allfiles"):
             #print(year)
             #print(cik)
             cik=file[:file.find("-")]
-            if (year,cik) in s:
+            if (cik,year) in s:
                 matchedstring=""
+                num=s[(cik,year)]
             #for i in s:
                 #year, cik=i
                 #if file[file.find("-")+1:file.find(".")]==year and file[:file.find("-")]==cik: 
@@ -80,8 +139,9 @@ for root, dirs, files in os.walk("/Users/lucy/Desktop/others/allfiles"):
                                 i+=1
                             break
                             
-                f.append(matchedstring.strip())
-                print(f)
+                f[num]=matchedstring.strip()
+
+print(f)
 
 
 
@@ -89,7 +149,9 @@ z=zip(l1,l2,l3,l4,l5,l6,l7,f)
 csv2="/Users/lucy/Desktop/assortedcodes/vectorfinal.csv"
 f_out2 = open(csv2, 'w')
 wr2 = csv.writer(f_out2)
-wr2.writerow(["filename","year","cik","posint","negint","posextlist","negextlist","filingdate"])
+wr2.writerow(["filename","year","cik","posl","negl","spreturns","roa","filedate"])
+
+#wr2.writerow(["filename","year","cik","posint","negint","posextlist","negextlist","filingdate"])
 for i in z: 
     wr2.writerow(i)
 
@@ -99,51 +161,73 @@ for i in z:
 Part 2: retrieve all filedates
 """ 
 
-dic={}
-with open(dir1,"r") as posfile: 
-    records=csv.reader(posfile)
-    i=0
-    for row in records:
-        if i==0: 
-            pass
-        i+=1
-        cik=row[7]
-        datadate=row[2]
-        cshoc=row[4]
-        eps=row[5]
-        prccd=row[6]
-        prcod=row[7]
-        gsector=row[9]
-        dic.update({(cik,datadate):(cshoc,eps,prccd,prcod,gsector)})
+
+# d="/Users/lucy/Desktop/assortedcodes/91e17c1ef257fb31.csv"
+
+# d=defaultdict(list)
+# with open(d,"r") as posfile: 
+#     records=csv.reader(posfile)
+#     i=0
+#     for row in records:
+#         if i==0: 
+#             pass
+#         i+=1
+#         cik=row[7]
+#         datadate=row[2]
+#         cshoc=row[4]
+#         eps=row[5]
+#         prccd=row[6]
+#         prcod=row[7]
+#         gsector=row[9]
+#         dic.update({(cik,datadate):(cshoc,eps,prccd,prcod,gsector)})
 
 
-l1=[]
-l2=[]
-l3=[]
-l4=[]
-l5=[]
-l6=[]
-l7=[]
-
-with open(csv2,"r") as e:
-    records=csv.reader(e)
-    i=0
-    for row in records:
-        if i==0: 
-            pass
-        year=row[1]
-        cik=row[2]
-        if (cik,year) in dic:
-            l1=[]
-            l2=[]
-            l3=[]
-            l4=[]
-            l5=[]
-            l6=[]
-            l7.append(dic[(cik:year)])
+# d=defaultdict(list)
+# with open(d,"r") as posfile: 
+#     records=csv.reader(posfile)
+#     i=0
+#     for row in records:
+#         if i==0: 
+#             pass
+#         i+=1
+#         cik=row[12]
+#         datadate=row[2]
+#         ni=row[11]
+#         at=row[10]
+#         ROA=ni/at
+#         dic.update({(cik,datadate):ROA})
 
 
-        #prcod
+
+
+# with open()
+# l1=[]
+# l2=[]
+# l3=[]
+# l4=[]
+# l5=[]
+# l6=[]
+# l7=[]
+
+# with open(csv2,"r") as e:
+#     records=csv.reader(e)
+#     i=0
+#     for row in records:
+#         if i==0: 
+#             pass
+#         year=row[1]
+#         cik=row[2]
+#         if (cik,year) in dic:
+#             l1=[]
+#             l2=[]
+#             l3=[]
+#             l4=[]
+#             l5=[]
+#             l6=[]
+#             l7.append(dic[(cik:year)])
+
+
+#         #prcod
 
 
 
@@ -155,61 +239,61 @@ with open(csv2,"r") as e:
 
 
 
-"""
+# """
 
-with open(dir1,"r") as posfile: 
-    records=csv.reader(posfile)
-    i=0
-    for row in records:
-        if i==0: 
-            pass
-        i+=1
-        cshoc=row[4]
-        eps=row[5]
+# with open(dir1,"r") as posfile: 
+#     records=csv.reader(posfile)
+#     i=0
+#     for row in records:
+#         if i==0: 
+#             pass
+#         i+=1
+#         cshoc=row[4]
+#         eps=row[5]
 
-        i+=1
-        year=row[1]
-        cik=row[2]
-        posint=row[3]
-        negint=row[4]
-        posextlist=row[5]
-        negextlist=row[6]
-
-
-
-d={}
-with open(dir1,"r") as posfile: 
-    records=csv.reader(posfile)
-    i=0
-    for row in records:
-        if i==0: 
-            pass
-        i+=1
-        cshoc=row[4]
-        eps=row[5]
+#         i+=1
+#         year=row[1]
+#         cik=row[2]
+#         posint=row[3]
+#         negint=row[4]
+#         posextlist=row[5]
+#         negextlist=row[6]
 
 
+
+# d={}
+# with open(dir1,"r") as posfile: 
+#     records=csv.reader(posfile)
+#     i=0
+#     for row in records:
+#         if i==0: 
+#             pass
+#         i+=1
+#         cshoc=row[4]
+#         eps=row[5]
 
 
 
 
 
 
-        i+=1
-        year=row[1]
-        cik=row[2]
-        posint=row[3]
-        negint=row[4]
-        posextlist=row[5]
-        negextlist=row[6]
 
-        d.update{(year,cik):(posint)}
 
-        posperf=row[0].lower()
-        if len(posperf.split(" "))>1: 
-            w1=posperf.split(" ")[0]
-            w2=posperf.split(" ")[1]
+#         i+=1
+#         year=row[1]
+#         cik=row[2]
+#         posint=row[3]
+#         negint=row[4]
+#         posextlist=row[5]
+#         negextlist=row[6]
 
-"""
+#         d.update{(year,cik):(posint)}
+
+#         posperf=row[0].lower()
+#         if len(posperf.split(" "))>1: 
+#             w1=posperf.split(" ")[0]
+#             w2=posperf.split(" ")[1]
+
+# """
 
 
---
+++ regression2.R
@@ -0,0 +1,112 @@
+library(readr)
+#install.packages("estimatr")
+#install.packages("margins")
+#install.packages("car")
+library(estimatr)
+library(margins)
+library(car)
+summary(regression1)
+reg = lm_robust(as.double(log(roa)) ~ as.double(posl)+as.double(negl)+as.double(spreturns), data = regression3)
+summary(reg)
+linearHypothesis(reg, c("as.double(posl)"), test = "F")
+linearHypothesis(reg, c("as.double(negl)"), test = "F")
+#linearHypothesis(reg, c("as.double(posl)"), test = "F")
+
+
+#as.double(posl)+ as.double(negl)+
+linearHypothesis(reg, c("as.double(posl)"), test = "F")
+linearHypothesis(reg, c("as.double(negl)"), test = "F")
+
+
+
+reg = lm_robust(as.double(spreturns) ~ as.double(posl)), data = regression3)
+summary(reg)
+linearHypothesis(reg, c("as.double(posl)"), test = "F")
+
+reg = lm_robust(as.double(spreturns) ~ as.double(negl), data = regression3)
+summary(reg)
+linearHypothesis(reg, c("as.double(negl)"), test = "F")
+
+reg1 = lm_robust(as.double(negl) ~ as.double(spreturns), data = regression2)
+summary(reg1)
+linearHypothesis(reg1, c("as.double(spreturns)"), test = "F")
+
+"""
+filename         year           cik      
+/Users/lucy/Desktop/others/newdictestn/newdic/1000180-2006mda.txt:   1   2012   : 550   832480 :   4  
+/Users/lucy/Desktop/others/newdictestn/newdic/1000180-2014mda.txt:   1   2011   : 549   1000228:   3  
+/Users/lucy/Desktop/others/newdictestn/newdic/1000209-2009mda.txt:   1   2001   : 545   1000232:   3  
+/Users/lucy/Desktop/others/newdictestn/newdic/1000209-2017mda.txt:   1   2009   : 535   1000753:   3  
+/Users/lucy/Desktop/others/newdictestn/newdic/1000227-2005mda.txt:   1   1998   : 534   1001082:   3  
+/Users/lucy/Desktop/others/newdictestn/newdic/1000228-1997mda.txt:   1   2015   : 521   1001606:   3  
+(Other)                                                          :9992   (Other):6764   (Other):9979  
+pos               neg             l            spreturns         posl               negl         
+Min.   :   0.00   Min.   :   0   Min.   :    28   16     : 550   Min.   :0.000000   Min.   :0.000000  
+1st Qu.:  25.00   1st Qu.:  36   1st Qu.:  3208   2.11   : 549   1st Qu.:0.006506   1st Qu.:0.008448  
+Median :  57.00   Median :  77   Median :  6430   -11.89 : 545   Median :0.009032   Median :0.011894  
+Mean   :  85.59   Mean   : 103   Mean   :  8634   26.46  : 535   Mean   :0.009686   Mean   :0.012624  
+3rd Qu.: 112.00   3rd Qu.: 139   3rd Qu.: 10786   28.58  : 534   3rd Qu.:0.011924   3rd Qu.:0.015861  
+Max.   :1903.00   Max.   :1839   Max.   :121176   1.38   : 521   Max.   :0.134831   Max.   :0.056471  
+(Other):6764                                        
+> reg = lm_robust(as.double(spreturns) ~ as.double(posl)+ as.double(negl), data = regression1)
+> summary(reg)
+
+Call:
+  lm_robust(formula = as.double(spreturns) ~ as.double(posl) + 
+              as.double(negl), data = regression1)
+
+Standard error type:  HC2 
+
+Coefficients:
+  Estimate Std. Error t value  Pr(>|t|) CI Lower CI Upper   DF
+(Intercept)        12.52     0.1652  75.782 0.000e+00    12.19   12.839 9995
+as.double(posl)   -30.19    14.5762  -2.071 3.835e-02   -58.77   -1.621 9995
+as.double(negl)    49.48    11.6222   4.257 2.086e-05    26.70   72.264 9995
+
+Multiple R-squared:  0.002076 ,	Adjusted R-squared:  0.001876 
+F-statistic: 9.094 on 2 and 9995 DF,  p-value: 0.0001133
+Linear hypothesis test
+
+Hypothesis:
+  as.double(posl) = 0
+
+Model 1: restricted model
+Model 2: as.double(spreturns) ~ as.double(posl) + as.double(negl)
+
+Res.Df Df      F  Pr(>F)  
+1   9996                    
+2   9995  1 4.2907 0.03835 *
+  ---
+  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+> 
+  > reg1 = lm_robust(as.double(negl) ~ as.double(spreturns), data = regression1)
+> summary(reg1)
+
+Call:
+  lm_robust(formula = as.double(negl) ~ as.double(spreturns), data = regression1)
+
+Standard error type:  HC2 
+
+Coefficients:
+  Estimate Std. Error t value  Pr(>|t|)  CI Lower  CI Upper   DF
+(Intercept)          1.214e-02  1.370e-04  88.644 0.0000000 1.187e-02 1.241e-02 9996
+as.double(spreturns) 3.753e-05  9.843e-06   3.813 0.0001381 1.824e-05 5.682e-05 9996
+
+Multiple R-squared:  0.001509 ,	Adjusted R-squared:  0.001409 
+F-statistic: 14.54 on 1 and 9996 DF,  p-value: 0.0001381
+Linear hypothesis test
+
+Hypothesis:
+  as.double(spreturns) = 0
+
+Model 1: restricted model
+Model 2: as.double(negl) ~ as.double(spreturns)
+
+Res.Df Df      F    Pr(>F)    
+1   9997                        
+2   9996  1 14.539 0.0001381 ***
+  ---
+  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+"""
+
